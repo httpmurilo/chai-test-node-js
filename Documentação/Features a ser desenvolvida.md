1b11dcc1b6c691fb0498a664d94ba8f2c701970a
Murilo Eduardo <muriloeduardo1997@outlook.com>
2020-11-05 15:59:09 -0300

    up

--- Documentação/Features a ser desenvolvida.md
+++
@@ -1,3 +0,0 @@
-1.DJE
-2. Importação dos dados do TJs
-3. Vantagem competitiva
--- src/app.js
+++ src/app.js
@@ -17,30 +17,10 @@ const path = require('path');
 mongoose.connect(config.connectionString);
 
 
-const TipoPendencia = require('./models/tipoPendencia');
 const Vara = require('./models/vara');
-const TypeDoc  = require('./models/tipoDoc');
-const Cliente= require('./models/Cliente');
-const PagDoc = require('./models/pagDoc');
-const tipoTarefa = require('./models/tipoTarefa');
-const tarefa = require('./models/task');
-const ProcessoParte = require('./models/ProcessoParte')
-const Processo = require('./models/Processo');
-const FinanceiroCliente = require('./models/FinanceiroCliente');
 
 const varaRota= require('./routers/vara-rota');
-const indexRota= require('./routers/index-rota');
-const tipoPendenciaRota= require('./routers/tipoPendencia-rota');
-const TipoDocRota= require('./routers/tipoDoc-rota');
-const ClienteRota = require('./routers/cliente-rota');
-const PagDocRota= require('./routers/pagdoc-rota');
-const tipoTarefaRota= require('./routers/tipoTarefa-rota');
-const TaskRota= require('./routers/tarefa-rota');
-const ParteProcessoRota= require('./routers/processo-parte-rota');
-const ProcessoRota= require('./routers/processo-rota');
-const FinanceiroClienteRota = require('./routers/financeiro-cliente-rota');
-//arquivo do log
-// create a write stream (in append mode)
+
 const  accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 app.use(morgan('dev', {
     skip: function (req, res) { return res.statusCode < 501 }
@@ -62,16 +42,6 @@ app.use(morgan('dev', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/',indexRota);
 app.use('/varas',varaRota);
-app.use('/tipoPendencias',tipoPendenciaRota);
-app.use('/tipoDoc', TipoDocRota);
-app.use('/clientes', ClienteRota);
-app.use('/documentos',PagDocRota);
-app.use('/tipoTarefas',tipoTarefaRota);
-app.use('/task',TaskRota);
-app.use('/partesProcesso',ParteProcessoRota);
-app.use('/processos', ProcessoRota);
-app.use('/financeiroCliente',FinanceiroClienteRota);
 
 module.exports = app;
